d299190c83129385de0fc448349c6b123ccec0f1
Eugene <63777417+nadProg@users.noreply.github.com>
2022-01-24 20:01:22 +0700

    Create mock data source files

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -4,69 +4,18 @@ const chalk = require(`chalk`);
 
 const fs = require(`fs`).promises;
 const dayjs = require(`dayjs`);
-const {ExitCode} = require(`../../constants`);
+const {ExitCode, FilePath} = require(`../../constants`);
 const {
   shuffle,
   getRandomInt,
   getRandomItem,
   getUniqueArray,
+  readContent,
 } = require(`../../utils`);
 
 const DEFAULT_COUNT = 1;
 
 const MAX_COUNT = 1000;
-
-const FILE_MOCKS_PATH = `mocks.json`;
-
-const TITLES = [
-  `Ёлки. История деревьев`,
-  `Как перестать беспокоиться и начать жить`,
-  `Как достигнуть успеха не вставая с кресла`,
-  `Обзор новейшего смартфона`,
-  `Лучшие рок-музыканты 20-века`,
-  `Как начать программировать`,
-  `Учим HTML и CSS`,
-  `Что такое золотое сечение`,
-  `Как собрать камни бесконечности`,
-  `Борьба с прокрастинацией`,
-  `Рок — это протест`,
-  `Самый лучший музыкальный альбом этого года`,
-];
-
-const SENTENCES = [
-  `Ёлки — это не просто красивое дерево. Это прочная древесина.`,
-  `Первая большая ёлка была установлена только в 1938 году.`,
-  `Вы можете достичь всего. Стоит только немного постараться и запастись книгами.`,
-  `Этот смартфон — настоящая находка. Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете.`,
-  `Золотое сечение — соотношение двух величин, гармоническая пропорция.`,
-  `Собрать камни бесконечности легко, если вы прирожденный герой.`,
-  `Освоить вёрстку несложно. Возьмите книгу новую книгу и закрепите все упражнения на практике.`,
-  `Бороться с прокрастинацией несложно. Просто действуйте. Маленькими шагами.`,
-  `Программировать не настолько сложно, как об этом говорят.`,
-  `Простые ежедневные упражнения помогут достичь успеха.`,
-  `Это один из лучших рок-музыкантов.`,
-  `Он написал больше 30 хитов.`,
-  `Из под его пера вышло 8 платиновых альбомов.`,
-  `Процессор заслуживает особого внимания. Он обязательно понравится геймерам со стажем.`,
-  `Рок-музыка всегда ассоциировалась с протестами. Так ли это на самом деле?`,
-  `Достичь успеха помогут ежедневные повторения.`,
-  `Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.`,
-  `Как начать действовать? Для начала просто соберитесь.`,
-  `Игры и программирование разные вещи. Не стоит идти в программисты, если вам нравятся только игры.`,
-  `Альбом стал настоящим открытием года. Мощные гитарные рифы и скоростные соло-партии не дадут заскучать.`,
-];
-
-const CATEGORIES = [
-  `Деревья`,
-  `За жизнь`,
-  `Без рамки`,
-  `Разное`,
-  `IT`,
-  `Музыка`,
-  `Кино`,
-  `Программирование`,
-  `Железо`,
-];
 
 const SentenceRestrict = {
   MIN: 1,
@@ -78,14 +27,14 @@ const DayRestrict = {
   MAX: 90,
 };
 
-const generateAnnounce = () =>
-  shuffle(SENTENCES)
+const generateAnnounce = (sentences) =>
+  shuffle(sentences)
     .slice(0, getRandomInt(SentenceRestrict.MIN, SentenceRestrict.MAX))
     .join(` `);
 
-const generateFullText = () =>
-  shuffle(SENTENCES)
-    .slice(0, getRandomInt(SentenceRestrict.MAX, SENTENCES.length - 1))
+const generateFullText = (sentences) =>
+  shuffle(sentences)
+    .slice(0, getRandomInt(SentenceRestrict.MAX, sentences.length - 1))
     .join(` `);
 
 const generateCreatedDate = () => {
@@ -93,15 +42,18 @@ const generateCreatedDate = () => {
   return dayjs().subtract(randomDay, `day`).toISOString();
 };
 
-const generateArticle = () => ({
-  title: getRandomItem(TITLES),
-  announce: generateAnnounce(),
-  fullText: generateFullText(),
+const generateArticle = ({titles, categories, sentences}) => ({
+  title: getRandomItem(titles),
+  announce: generateAnnounce(sentences),
+  fullText: generateFullText(sentences),
   createdDate: generateCreatedDate(),
-  category: getUniqueArray(CATEGORIES),
+  category: getUniqueArray(categories),
 });
 
-const generateArticles = (count) => Array(count).fill({}).map(generateArticle);
+const generateArticles = (count, sourceData) =>
+  Array(count)
+    .fill({})
+    .map(() => generateArticle(sourceData));
 
 module.exports = {
   name: `--generate`,
@@ -114,15 +66,29 @@ module.exports = {
       process.exit(ExitCode.ERROR);
     }
 
-    const content = JSON.stringify(generateArticles(count));
+    const [titles, categories, sentences] = await Promise.all([
+      readContent(FilePath.TITLES),
+      readContent(FilePath.CATEGORIES),
+      readContent(FilePath.SENTENCES),
+    ]);
+
+    const content = JSON.stringify(
+        generateArticles(count, {titles, categories, sentences})
+    );
 
     console.info(chalk.yellowBright(`Дождитесь окончания записи файла...`));
 
     try {
-      await fs.writeFile(FILE_MOCKS_PATH, content);
-      console.info(chalk.green(`Операция завершена успешно. Данные записаны в файл ${FILE_MOCKS_PATH}.`));
+      await fs.writeFile(FilePath.MOCKS, content);
+      console.info(
+          chalk.green(
+              `Операция завершена успешно. Данные записаны в файл ${FilePath.MOCKS}.`
+          )
+      );
     } catch (error) {
-      console.error(chalk.red(`Операция завершена с ошибкой. Данные не сохранены.`));
+      console.error(
+          chalk.red(`Операция завершена с ошибкой. Данные не сохранены.`)
+      );
       process.exit(ExitCode.ERROR);
     }
   },
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,7 @@
-'use strict';
+"use strict";
+
+const fs = require(`fs`).promises;
+const chalk = require(`chalk`);
 
 const getRandomInt = (min, max) => {
   min = Math.ceil(min);
@@ -22,9 +25,20 @@ const getUniqueArray = (items) => {
   return shuffle(items).slice(0, getRandomInt(0, items.length - 1));
 };
 
+const readContent = async (filePath) => {
+  try {
+    const content = await fs.readFile(filePath, `utf8`);
+    return content.trim().split(`\n`);
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
+};
+
 module.exports = {
   shuffle,
   getRandomInt,
   getRandomItem,
   getUniqueArray,
+  readContent,
 };

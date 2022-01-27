ed3719a41ed56240ba23a073b9c4d7f70e92e7e1
Eugene <63777417+nadProg@users.noreply.github.com>
2022-01-27 22:31:34 +0700

    Update error handling in templates

--- src/express/routes/error-routes.js
+++ src/express/routes/error-routes.js
@@ -1,19 +1,16 @@
 'use strict';
 
 const {Router} = require(`express`);
-const {HttpCode} = require(`../../constants`);
+const {Error} = require(`../../constants`);
 
 const articlesRoutes = new Router();
 
 articlesRoutes.get(`/404`, (req, res) => res.render(`error`, {
-  errorCode: HttpCode.NOT_FOUND,
-  errorMessage: `Похоже ошиблись адресом`
+  error: Error.CLIENT
 }));
 
 articlesRoutes.get(`/500`, (req, res) => res.render(`error`, {
-  errorCode: HttpCode.INTERNAL_SERVER_ERROR,
-  errorMessage: `Что-то пошло не так`,
-  errorText: `Причин может быть много: сервер не выдержал нагрузку или в коде ошибка. Попробуйте повторить попытку позже.`
+  error: Error.SERVER
 }));
 
 module.exports = articlesRoutes;

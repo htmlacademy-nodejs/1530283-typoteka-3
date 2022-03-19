b369467e028a55dd24c307749cebcde598f170c0
Eugene Skrobov <skrobov.ev@gmail.com>
2022-03-19 20:49:05 +0700

    Refactor lib sequelize

--- src/service/lib/sequelize.js
+++ src/service/lib/sequelize.js
@@ -4,22 +4,30 @@ const Sequelize = require(`sequelize`);
 
 const {DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT} = process.env;
 
-const somethingIsNotDefined = [DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT].some((value) => value === undefined);
+const DB_DIALECT = `postgres`;
+
+const DB_POOL_SETTINGS = {
+  min: 0,
+  max: 5,
+  acquire: 10000,
+  idle: 10000,
+};
+
+const somethingIsNotDefined = [
+  DB_NAME,
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_PORT,
+].some((value) => value === undefined);
 
 if (somethingIsNotDefined) {
   throw new Error(`One or more environmental variables are not defined`);
 }
 
-module.exports = new Sequelize(
-    DB_NAME, DB_USER, DB_PASSWORD, {
-      host: DB_HOST,
-      port: DB_PORT,
-      dialect: `postgres`,
-      pool: {
-        max: 5,
-        min: 0,
-        acquire: 10000,
-        idle: 10000
-      }
-    }
-);
+module.exports = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  port: DB_PORT,
+  dialect: DB_DIALECT,
+  pool: DB_POOL_SETTINGS,
+});

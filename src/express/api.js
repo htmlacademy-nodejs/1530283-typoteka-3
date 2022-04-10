b6b1b39c07d2f31f84abc648ce58f816e785d3aa
Eugene Skrobov <skrobov.ev@gmail.com>
2022-04-10 16:28:20 +0700

    Refactor api

--- src/express/api.js
+++ src/express/api.js
@@ -3,9 +3,10 @@
 const axios = require(`axios`);
 const {HttpMethod} = require(`../constants`);
 
+const DEFAULT_API_PORT = 3000;
 const TIMEOUT = 1000;
 
-const port = process.env.API_PORT || 3000;
+const port = process.env.API_PORT || DEFAULT_API_PORT;
 const defaultUrl = `http://localhost:${port}/api/`;
 
 class API {

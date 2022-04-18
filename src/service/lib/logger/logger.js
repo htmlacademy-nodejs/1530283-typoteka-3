e0dfe1f94ba45256b3b90cbbf8773f50d6aaadcc
Eugene Skrobov <skrobov.ev@gmail.com>
2022-04-18 22:29:31 +0700

    Improve logger initializing

--- src/service/lib/logger/logger.js
+++ src/service/lib/logger/logger.js
@@ -1,11 +1,19 @@
 "use strict";
 
+const fs = require(`fs`);
 const path = require(`path`);
 const pino = require(`pino`);
 
 const {Environment} = require(`../../../constants`);
 
 const DEFAULT_LOG_LEVEL = `info`;
+const LOG_FILENAME = `api.log`;
+
+const logDirectory = path.resolve(__dirname, `../../../../logs`);
+
+if (!fs.existsSync(logDirectory)) {
+  fs.mkdirSync(logDirectory);
+}
 
 const isDevMode = process.env.NODE_ENV === Environment.DEVELOPMENT;
 
@@ -15,7 +23,7 @@ const logger = pino({
   transport: {
     target: isDevMode ? `./pino-pretty-target` : `pino/file`,
     options: {
-      destination: !isDevMode ? path.resolve(__dirname, `../../../../logs/api.log`) : undefined,
+      destination: !isDevMode ? path.resolve(logDirectory, LOG_FILENAME) : undefined,
     }
   }
 });

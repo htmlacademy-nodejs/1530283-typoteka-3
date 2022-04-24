5805930e6cbc0df5bdcfd75c2e41b6f9b4690ac1
Eugene Skrobov <skrobov.ev@gmail.com>
2022-04-24 23:51:45 +0700

    Update eslint config

--- src/service/models/comment.js
+++ src/service/models/comment.js
@@ -2,13 +2,13 @@
 
 const {DataTypes, Model} = require(`sequelize`);
 
-class Comment extends Model {}
+class Comment extends Model {
+}
 
 const define = (sequelize) =>
   Comment.init(
       {
         text: {
-        // eslint-disable-next-line new-cap
           type: DataTypes.STRING(255),
         },
       },
